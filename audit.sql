909ef2a2f982909d297035bc2fcdc06bc993f322
Hagen Hübel <hhuebel@itinance.com>
2020-11-08 21:45:19 +0100

    save space via lookup-tables and enums

--- audit.sql
+++ audit.sql
@@ -12,12 +12,142 @@
 --
 -- Should really be converted into a relocatable EXTENSION, with control and upgrade files.
 
+
+
 CREATE EXTENSION IF NOT EXISTS hstore;
 
 CREATE SCHEMA audit;
 REVOKE ALL ON SCHEMA audit FROM public;
 
 COMMENT ON SCHEMA audit IS 'Out-of-table audit/history logging tables and trigger functions';
+
+--
+-- Table logged_schemas
+--
+
+CREATE SEQUENCE audit.seq_logged_schemas;
+CREATE TABLE audit.logged_schemas (
+    id smallint NOT NULL PRIMARY KEY DEFAULT nextval('audit.seq_logged_schemas'),
+    name TEXT NOT NULL UNIQUE,
+    creation_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+ALTER SEQUENCE audit.seq_logged_schemas OWNED BY audit.logged_schemas.id;
+CREATE INDEX "idx_logged_schemas_name" ON audit.logged_schemas ("name");
+CREATE INDEX "idx_logged_schemas_creation_date" ON audit.logged_schemas ("creation_date");
+
+--
+-- Table logged_tables
+--
+
+CREATE SEQUENCE audit.seq_logged_tables;
+CREATE TABLE audit.logged_tables (
+    id smallint NOT NULL PRIMARY KEY DEFAULT nextval('audit.seq_logged_tables'),
+    name TEXT NOT NULL UNIQUE,
+    creation_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+ALTER SEQUENCE audit.seq_logged_tables OWNED BY audit.logged_tables.id;
+CREATE INDEX "idx_logged_tables_name" ON audit.logged_tables ("name");
+CREATE INDEX "idx_logged_tables_creation_date" ON audit.logged_tables ("creation_date");
+
+--
+-- Table logged_applications
+--
+
+CREATE SEQUENCE audit.seq_logged_applications;
+CREATE TABLE audit.logged_applications (
+    id smallint NOT NULL PRIMARY KEY DEFAULT nextval('audit.seq_logged_applications'),
+    name TEXT NOT NULL UNIQUE,
+    creation_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+ALTER SEQUENCE audit.seq_logged_applications OWNED BY audit.logged_applications.id;
+CREATE INDEX "idx_logged_applications_name" ON audit.logged_applications ("name");
+CREATE INDEX "idx_logged_applications_creation_date" ON audit.logged_applications ("creation_date");
+
+--
+-- Table logged_users
+--
+
+CREATE SEQUENCE audit.seq_logged_users;
+CREATE TABLE audit.logged_users (
+    id smallint NOT NULL PRIMARY KEY DEFAULT nextval('audit.seq_logged_users'),
+    name TEXT NOT NULL UNIQUE,
+    creation_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+ALTER SEQUENCE audit.seq_logged_users OWNED BY audit.logged_users.id;
+CREATE INDEX "idx_logged_users_name" ON audit.logged_users ("name");
+CREATE INDEX "idx_logged_users_creation_date" ON audit.logged_users ("creation_date");
+
+--
+-- Function getSchemaId
+--
+
+CREATE OR REPLACE FUNCTION audit.getSchemaId(_name TEXT) RETURNS SMALLINT AS $$
+DECLARE schemaId SMALLINT;
+BEGIN
+    SELECT INTO schemaId id FROM audit.logged_schemas WHERE name=_name;
+
+    IF schemaId IS NULL THEN
+        INSERT INTO audit.logged_schemas (name) VALUES (_name) RETURNING id INTO schemaId;
+    END IF;
+
+    RETURN schemaId;
+END; $$
+LANGUAGE PLPGSQL;
+SELECT audit.getSchemaId('public');
+
+--
+-- Function getTableId
+--
+
+CREATE OR REPLACE FUNCTION audit.getTableId(_name TEXT) RETURNS SMALLINT AS $$
+DECLARE tableId SMALLINT;
+BEGIN
+    SELECT INTO tableId id FROM audit.logged_tables WHERE name=_name;
+
+    IF tableId IS NULL THEN
+        INSERT INTO audit.logged_tables (name) VALUES (_name) RETURNING id INTO tableId;
+    END IF;
+
+    RETURN tableId;
+END; $$
+LANGUAGE PLPGSQL;
+
+
+--
+-- Function getApplicationId
+--
+
+CREATE OR REPLACE FUNCTION audit.getApplicationId(_name TEXT) RETURNS SMALLINT AS $$
+DECLARE applicationId SMALLINT;
+BEGIN
+    SELECT INTO applicationId id FROM audit.logged_applications WHERE name=_name;
+
+    IF applicationId IS NULL THEN
+        INSERT INTO audit.logged_applications (name) VALUES (_name) RETURNING id INTO applicationId;
+    END IF;
+
+    RETURN applicationId;
+END; $$
+LANGUAGE PLPGSQL;
+
+--
+-- Function getUserId
+--
+
+CREATE OR REPLACE FUNCTION audit.getUserId(_name TEXT) RETURNS SMALLINT AS $$
+DECLARE userId SMALLINT;
+BEGIN
+    SELECT INTO userId id FROM audit.logged_users WHERE name=_name;
+
+    IF userId IS NULL THEN
+        INSERT INTO audit.logged_users (name) VALUES (_name) RETURNING id INTO userId;
+    END IF;
+
+    RETURN userId;
+END; $$
+LANGUAGE PLPGSQL;
+
+CREATE TYPE audit.logged_action AS ENUM ('INSERT', 'UPDATE', 'DELETE', 'TRUNCATE');
 
 --
 -- Audited data. Lots of information is available, it's just a matter of how much
@@ -36,21 +166,22 @@ COMMENT ON SCHEMA audit IS 'Out-of-table audit/history logging tables and trigge
 -- you're interested in, into a temporary table where you CREATE any useful
 -- indexes and do your analysis.
 --
+DROP TABLE IF EXISTS audit.logged_actions;
 CREATE TABLE audit.logged_actions (
     event_id bigserial primary key,
-    schema_name text not null,
-    table_name text not null,
+    schema_id smallint not null references audit.logged_schemas(id),
+    table_id smallint not null references audit.logged_tables(id),
     relid oid not null,
-    session_user_name text,
+    session_user_id smallint not null references audit.logged_users(id),
     action_tstamp_tx TIMESTAMP WITH TIME ZONE NOT NULL,
     action_tstamp_stm TIMESTAMP WITH TIME ZONE NOT NULL,
     action_tstamp_clk TIMESTAMP WITH TIME ZONE NOT NULL,
     transaction_id bigint,
-    application_name text,
+    application_id smallint not null references audit.logged_applications(id),
     client_addr inet,
     client_port integer,
     client_query text,
-    action TEXT NOT NULL CHECK (action IN ('I','D','U', 'T')),
+    action audit.logged_action NOT NULL,
     row_data hstore,
     changed_fields hstore,
     statement_only boolean not null
@@ -60,10 +191,10 @@ REVOKE ALL ON audit.logged_actions FROM public;
 
 COMMENT ON TABLE audit.logged_actions IS 'History of auditable actions on audited tables, from audit.if_modified_func()';
 COMMENT ON COLUMN audit.logged_actions.event_id IS 'Unique identifier for each auditable event';
-COMMENT ON COLUMN audit.logged_actions.schema_name IS 'Database schema audited table for this event is in';
-COMMENT ON COLUMN audit.logged_actions.table_name IS 'Non-schema-qualified table name of table event occured in';
+COMMENT ON COLUMN audit.logged_actions.schema_id IS 'Database schema audited table for this event is in';
+COMMENT ON COLUMN audit.logged_actions.table_id IS 'Non-schema-qualified table name of table event occured in';
 COMMENT ON COLUMN audit.logged_actions.relid IS 'Table OID. Changes with drop/create. Get with ''tablename''::regclass';
-COMMENT ON COLUMN audit.logged_actions.session_user_name IS 'Login / session user whose statement caused the audited event';
+COMMENT ON COLUMN audit.logged_actions.session_user_id IS 'Login / session user whose statement caused the audited event';
 COMMENT ON COLUMN audit.logged_actions.action_tstamp_tx IS 'Transaction start timestamp for tx in which audited event occurred';
 COMMENT ON COLUMN audit.logged_actions.action_tstamp_stm IS 'Statement start timestamp for tx in which audited event occurred';
 COMMENT ON COLUMN audit.logged_actions.action_tstamp_clk IS 'Wall clock time at which audited event''s trigger call occurred';
@@ -71,12 +202,16 @@ COMMENT ON COLUMN audit.logged_actions.transaction_id IS 'Identifier of transact
 COMMENT ON COLUMN audit.logged_actions.client_addr IS 'IP address of client that issued query. Null for unix domain socket.';
 COMMENT ON COLUMN audit.logged_actions.client_port IS 'Remote peer IP port address of client that issued query. Undefined for unix socket.';
 COMMENT ON COLUMN audit.logged_actions.client_query IS 'Top-level query that caused this auditable event. May be more than one statement.';
-COMMENT ON COLUMN audit.logged_actions.application_name IS 'Application name set when this audit event occurred. Can be changed in-session by client.';
-COMMENT ON COLUMN audit.logged_actions.action IS 'Action type; I = insert, D = delete, U = update, T = truncate';
+COMMENT ON COLUMN audit.logged_actions.application_id IS 'Application name set when this audit event occurred. Can be changed in-session by client.';
+COMMENT ON COLUMN audit.logged_actions.action IS 'Action type; INSERT, UPDATE, DELETE, TRUNCATE';
 COMMENT ON COLUMN audit.logged_actions.row_data IS 'Record value. Null for statement-level trigger. For INSERT this is the new tuple. For DELETE and UPDATE it is the old tuple.';
 COMMENT ON COLUMN audit.logged_actions.changed_fields IS 'New values of fields changed by UPDATE. Null except for row-level UPDATE events.';
 COMMENT ON COLUMN audit.logged_actions.statement_only IS '''t'' if audit event is from an FOR EACH STATEMENT trigger, ''f'' for FOR EACH ROW';
 
+CREATE INDEX logged_actions_schema_id_idx ON audit.logged_actions(schema_id);
+CREATE INDEX logged_actions_table_id_idx ON audit.logged_actions(table_id);
+CREATE INDEX logged_actions_session_user_id_idx ON audit.logged_actions(session_user_id);
+CREATE INDEX logged_actions_application_id_idx ON audit.logged_actions(application_id);
 CREATE INDEX logged_actions_relid_idx ON audit.logged_actions(relid);
 CREATE INDEX logged_actions_action_tstamp_tx_stm_idx ON audit.logged_actions(action_tstamp_stm);
 CREATE INDEX logged_actions_action_idx ON audit.logged_actions(action);
@@ -96,19 +231,19 @@ BEGIN
 
     audit_row = ROW(
         nextval('audit.logged_actions_event_id_seq'), -- event_id
-        TG_TABLE_SCHEMA::text,                        -- schema_name
-        TG_TABLE_NAME::text,                          -- table_name
+        audit.getSchemaId(TG_TABLE_SCHEMA::text),                        -- schema_name
+        audit.gettableid(TG_TABLE_NAME::text),                          -- table_name
         TG_RELID,                                     -- relation OID for much quicker searches
-        session_user::text,                           -- session_user_name
+        audit.getUserId( session_user::text),                           -- session_user_name
         current_timestamp,                            -- action_tstamp_tx
         statement_timestamp(),                        -- action_tstamp_stm
         clock_timestamp(),                            -- action_tstamp_clk
         txid_current(),                               -- transaction ID
-        current_setting('application_name'),          -- client application
+        audit.getApplicationId( current_setting('application_name')),          -- client application
         inet_client_addr(),                           -- client_addr
         inet_client_port(),                           -- client_port
         current_query(),                              -- top-level query or queries (if multistatement) from client
-        substring(TG_OP,1,1),                         -- action
+        TG_OP::audit.logged_action,                         -- action
         NULL, NULL,                                   -- row_data, changed_fields
         'f'                                           -- statement_only
         );
@@ -250,3 +385,14 @@ ORDER BY schema, auditedtable;
 COMMENT ON VIEW audit.tableslist IS $body$
 View showing all tables with auditing set up. Ordered by schema, then table.
 $body$;
+
+CREATE OR REPLACE VIEW audit.view_logged_actions AS
+SELECT l.event_id, schemas.name AS schema, tables.name AS table, users.name AS session_user,
+       applications.name AS application, l.action,
+       l.action_tstamp_tx, l.action_tstamp_stm, l.action_tstamp_clk, l.relid, l.transaction_id,
+       l.client_addr, l.client_port, l.client_query, l.row_data, l.changed_fields, l.statement_only
+FROM audit.logged_actions l
+INNER JOIN audit.logged_schemas schemas ON l.schema_id = schemas.id
+INNER JOIN audit.logged_tables tables ON l.table_id = tables.id
+INNER JOIN audit.logged_users users ON l.session_user_id = users.id
+INNER JOIN audit.logged_applications applications ON l.application_id = applications.id;
